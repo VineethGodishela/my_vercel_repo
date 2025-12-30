6bf7e555416e70f6d7a0b41b177deb5c05598b28
G Vineeth <vineethgodishela@gmail.com>
2025-12-30 12:07:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ export default function App() {
      <h1>Welcome to App</h1>
      <h2>Wleocme to React app</h2>
      <h3>Welcome to React eployement using Vercel and vs code and git hub</h3>
+    < h1>Welcome to App</h1>
+     <h2>Wleocme to React app</h2>
+     <h3>Welcome to React eployement using Vercel and vs code and git hub</h3>
     </div>
   )
 }
