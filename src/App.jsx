e562b6239c222728cc0c46a0158f72f123bb2a4a
G Vineeth <vineethgodishela@gmail.com>
2025-12-30 12:02:01 +0530

    App updated Again

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Welcome to react Deployment</h1>
+     <h1>Welcome to App</h1>
+     <h2>Wleocme to React app</h2>
+     <h3>Welcome to React eployement using Vercel and vs code and git hub</h3>
     </div>
   )
 }
